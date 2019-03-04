698130aac37caf61e9490119428e2a68a2c7e029
svaskeli <svaskeli@student.42.fr>
2019-03-04 11:15:16 +0100

    fixed gnl

--- main.c
+++ main.c
@@ -8,7 +8,11 @@ int main (int ac, char **av)
 
     fd = open(av[1], O_RDONLY);
     while (get_next_line(fd, &str) == 1)
-        ft_printf("%s\n", *str);
+    {
+        ft_printf("%s\n", str);
+        free(str);
+    }
+    free(str);
     close (fd);
     return (0);
 }
--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -12,13 +12,13 @@
 
 #include "libft.h"
 
-static int	ft_get_line(const int fd, char **line, char *str, char **mem)
+static int	get_line(const int fd, char **line, char *str, char **mem)
 {
 	if (ft_strchr(str, '\n') == NULL)
 	{
 		if (!(*line = ft_strjoinfree_s1(*line, str)))
 			return (-1);
-		ft_strclr(str);
+		ft_strclr(mem[fd]);
 	}
 	else
 	{
@@ -31,44 +31,69 @@ static int	ft_get_line(const int fd, char **line, char *str, char **mem)
 	return (0);
 }
 
-static int	ft_get_from_mem(const int fd, char **line, char **mem, char *str)
+static int	get_mem_line(const int fd, char **line, char *str, char **mem)
 {
-	if (mem[fd])
+	char *tmp;
+
+	if (ft_strchr(mem[fd], '\n') == NULL)
 	{
-		if (ft_get_line(fd, line, mem[fd], mem) == 1)
-		{
-			ft_freestr(str);
-			return (1);
-		}
+		if (!(*line = ft_strjoinfree_s1(*line, mem[fd])))
+			return (-1);
+		ft_strclr(mem[fd]);
+	}
+	else
+	{
+		tmp = ft_strchr(mem[fd], '\n') + 1;
+		*ft_strchr(mem[fd], '\n') = '\0';
+		if (!(*line = ft_strjoinfree_s1(*line, mem[fd])))
+			return (-1);
+		mem[fd] = tmp;
+		ft_freestr(str);
+		return (1);
 	}
 	return (0);
 }
 
-int	get_next_line(const int fd, char **line)
+static int	check_if_read(char **line, int k, char *str)
+{
+	if (k == 0 && !*line[0])
+	{
+		ft_freestr(str);
+		return (0);
+	}
+	else if (k == 0 && *line[0])
+	{
+		ft_freestr(str);
+		return (1);
+	}
+	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	static char	*mem[MAX_FD];
-	char		*s;
+	char		*str;
 	int			k;
 
-	if (BUFF_SIZE < 1 || line == NULL || fd < 0 || !(s = ft_strnew(BUFF_SIZE)))
+	if (BUFF_SIZE < 1 || line == NULL || !(str = ft_strnew(BUFF_SIZE)))
 		return (-1);
-	if (fd >= MAX_FD || !(*line = ft_strnew(BUFF_SIZE)))
+	if (fd < 0 || fd >= MAX_FD || !(*line = ft_strnew(BUFF_SIZE)))
 		return (-1);
 	k = 1;
-	if (ft_get_from_mem(fd, line, mem, s))
-		return (1);
+	if (mem[fd])
+		if (get_mem_line(fd, line, str, mem) == 1)
+			return (1);
 	while (k > 0)
 	{
-		if ((k = read(fd, s, BUFF_SIZE)) == -1)
+		if ((k = read(fd, str, BUFF_SIZE)) == -1)
 			return (-1);
-		if (ft_get_line(fd, line, s, mem) == 1)
+		if (get_line(fd, line, str, mem) == 1)
+			return (1);
+		if (check_if_read(line, k, str) == 1)
 			return (1);
 		else
-			ft_freestr(s);
-		if (k == 0 && !*line[0])
 			return (0);
-		else if (k == 0 && *line[0])
-			return (1);
 	}
+	ft_freestr(str);
 	return (-1);
 }
